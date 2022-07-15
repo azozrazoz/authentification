2921d0fd90903c5480a4271d69675602a09a618a
Дошан Есмаганбет <doshan.de@gmail.com>
2022-07-15 21:50:46 +0600

    second commit

--- calculator/index.php
+++ calculator/index.php
@@ -7,20 +7,22 @@
     abstract class Operation
     {
         public $code;
+        public $name;
         public $result;
 
         public function getValue() {
-            print("$name $result");
+            print($this.__toString());
         }
 
-        public function setValue() {
-            $result = 
+        public function __toString() {
+            return $this->$name . $this.$result;
         }
     }
 
     class Sum extends Operation
     {
         public $code = "1";
+        public $name = 'Сумма';
 
         public function __constructor(int $x, int $y){
             $result = $x + $y;
@@ -30,6 +32,7 @@
     class Subtract extends Operation
     {
         public $code = "2";
+        public $name = 'Вычитание';
 
         public function __constructor(int $x, int $y){
             $result = $x - $y;
@@ -39,6 +42,7 @@
     class Multiply extends Operation
     {
         public $code = "3";
+        public $name = 'Умножение';
 
         public function __constructor(int $x, int $y){
             $result = $x * $y;
@@ -48,6 +52,7 @@
     class Degree extends Operation
     {
         public $code = "4";
+        public $name = 'Деление';
 
         public function __constructor(int $x, int $y){
             $result = $x / $y;
@@ -57,6 +62,7 @@
     class Root extends Operation
     {
         public $code = "5";
+        public $name = 'Корень';
 
         public function __constructor(int $x, int $y){
             $root1 = $x ** 0.5;
@@ -65,7 +71,7 @@
         }
     }
 
-    function math() {
+    function main() {
         $num_1 = $_POST['num1'];
         $num_2 = $_POST['num2'];
         $op_index = $_POST['operation'];
@@ -74,35 +80,36 @@
         $multiply = new Multiply($num_1, $num_2);
         $degree = new Degree($num_1, $num_2);
         $root = new Root($num_1, $num_2);
-        $op = array(1 => $sum, 2 => $subtract, 3 => $multiply, 4 = $degree, 5 = $root);
+        $op = array(1 => $sum, 2 => $subtract, 3 => $multiply, 4 => $degree, 5 => $root);
 
-        switch($op_index){
-            case 1:
-                $result = $num_1 + $num_2;
-                print("Сложение: $result<br/>");
-                break;
-            case 2:
-                $result = $num_1 - $num_2;
-                print("Вычитание: $result<br/>");
-                break;
-            case 3:
-                $result = $num_1 * $num_2;
-                print("Умножение: $result<br/>");
-                break;
-            case 4:
-                $result = $num_1 / $num_2;
-                print("Деление: $result<br/>");
-                break;
-            case 5:
-                $num_1 = pow($num_1, 0.5);
-                $num_2 = $num_2 ** 0.5;
+        print($op[$op_index]->getValue());
+
+        // switch($op_index){
+        //     case 1:                
+        //         print($op[$op_index]->getValue());
+        //         break;
+        //     case 2:
+        //         $result = $num_1 - $num_2;
+        //         print("Вычитание: $result<br/>");
+        //         break;
+        //     case 3:
+        //         $result = $num_1 * $num_2;
+        //         print("Умножение: $result<br/>");
+        //         break;
+        //     case 4:
+        //         $result = $num_1 / $num_2;
+        //         print("Деление: $result<br/>");
+        //         break;
+        //     case 5:
+        //         $num_1 = pow($num_1, 0.5);
+        //         $num_2 = $num_2 ** 0.5;
                 
-                print("Корень первого числа: $num_1<br/>");
-                print("Корень второго числа: $num_2<br/>");
-                break;
-            default:
-            break;
-        }
+        //         print("Корень первого числа: $num_1<br/>");
+        //         print("Корень второго числа: $num_2<br/>");
+        //         break;
+        //     default:
+        //     break;
+        // }
     }
 ?>
 
@@ -119,7 +126,7 @@
     <div class="box">
         <div style="font-size: 30px;">
             <?
-                math(); 
+                main(); 
             ?>
         </div>
 
